c2d15b0c4aac41d3bd2fa058a768fbfe38e526be
Константин Дьяченко <konard@me.com>
2019-12-03 10:16:20 +0300

    Initial code commit.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PegasusSignificantWhitespaceExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var source = @"a = b
+if a:
+    a = b
+    if q:
+        a = z
+        d = f
+    b = c
+    def q:
+        a = c
+c = d";
+
+            try
+            {
+                var result = new SignificantWhitespaceParser().Parse(source);
+                Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
